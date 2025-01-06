63aeca2eae74a6e2f192d52e4174fdb82d23e19f
Nandan T S <nanven9@gmail.com>
2025-01-06 19:26:38 +0530

    Refactor Bundler  logic to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,47 @@
-import { useEffect, useRef, useState } from 'react';
-import './App.css'
-import * as esbuild from "esbuild-wasm";
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
-import CodeEditor from './components/CodeEditor';
-import { REACT_TEMPLATE } from './templates/REACT_TEMPLATE';
-import { editor } from 'monaco-editor';
-import 'bulmaswatch/cosmo/bulmaswatch.min.css';
-import Preview from './components/Preview';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+import CodeEditor from "./components/CodeEditor";
+import { REACT_TEMPLATE } from "./templates/REACT_TEMPLATE";
+import { editor } from "monaco-editor";
+import "bulmaswatch/cosmo/bulmaswatch.min.css";
+import Preview from "./components/Preview";
+import bundle from "./bundler";
 
 function App() {
+    const [code, setCode] = useState("");
 
-  const [code,setCode]=useState('');
+    const initializeReactApp = async () => {
+      try{
+        await bundleCode(REACT_TEMPLATE);
+      }
+      catch(err){}
+    };
 
-  const ref = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        (async()=>{await initializeReactApp()})();
+    }, []);
 
-  
+    const bundleCode = async (code: string) => {
+        const transpiledCode = await bundle(code);
+        setCode(transpiledCode || "");
+    };
 
-  const startService=async()=>{
+    return (
+        <div>
+            <CodeEditor
+                onChange={(
+                    value: string | undefined,
+                    ev: editor.IModelContentChangedEvent
+                ): void => {
+                    bundleCode(value || "");
+                }}
+                initialValue={REACT_TEMPLATE}
+            ></CodeEditor>
 
-    await esbuild.initialize({
-        worker: true,
-        wasmURL: "https://unpkg.com/esbuild-wasm@0.24.2/esbuild.wasm",
-    });
-
-  }
-
-  useEffect(()=>{
-     (async () => {
-         try {
-            await startService();
-            transpileCode(REACT_TEMPLATE);
-             
-         } catch (err) {
-             
-         }
-     })();
-    
-  },[])
-
-  const transpileCode=async(code:string)=>{
- 
-    try{
-      let result = await esbuild.build({
-        entryPoints:['index.js'],
-        bundle:true,
-        write:false,
-        plugins:[unpkgPathPlugin(), fetchPlugin(code)],
-        define:{
-          'process.env.NODE_ENV':'"production"',
-          global:'window'
-        }
-      });
-      
-      setCode(result.outputFiles[0].text);
-
-    }
-    catch(err){
-      console.log(err)
-    }
-    
-  }
-
-  
-  return (
-      <div ref={ref}>
-
-        <CodeEditor onChange={(value: string | undefined, ev: editor.IModelContentChangedEvent):void=>{
-          transpileCode(value || '')
-        }} initialValue={REACT_TEMPLATE}></CodeEditor> 
-        
-        <Preview code={code}></Preview>
-         
-      </div>
-  );
+            <Preview code={code}></Preview>
+        </div>
+    );
 }
 
-export default App
+export default App;
